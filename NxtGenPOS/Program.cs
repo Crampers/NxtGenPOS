5c0487f92bfdd029c61055ae5a559c41094972d3
Crampers <Crampers@users.noreply.github.com>
2015-09-27 16:14:10 +0200

    Created the overall classes

--- NxtGenPOS/Program.cs
+++ NxtGenPOS/Program.cs
@@ -10,7 +10,48 @@ namespace NxtGenPOS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            
+        }
+    }
+
+    class ProductCatalog
+    {
+         
+    }
+
+    class Register
+    {
+         
+    }
+
+    class Sale
+    {
+        private SalesLineItem sl;
+
+        public void makePayment()
+        {
+            
+        }
+
+        public void makeLineItem()
+        {
+            sl.create(5);
+        }
+    }
+
+    class Payment
+    {
+        public void create()
+        {
+
+        }
+    }
+
+    class SalesLineItem
+    {
+        public void create(int quantity)
+        {
+            
         }
     }
 }
